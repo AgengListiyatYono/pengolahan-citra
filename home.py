67ba07e039b3c47b6c915f447eb403e14cb47481
AgengListiyatYono <115475428+AgengListiyatYono@users.noreply.github.com>
2024-05-12 20:09:52 +0700

    push pengcit

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Profile')
+st.subheader('Nama : Ageng Listiyat Yono')
+st.subheader('NIM : 312210082')
+st.subheader('Kelas : TI.22.B1')
+st.subheader('Prodi : Teknik Infomatika')
+
+st.image("./img/ageng.jpeg", caption="Ageng", use_column_width="Always", width=200)
+
+
+st.link_button("Instagram", "https://www.instagram.com/agboy1108/")
+st.link_button("Github", "https://github.com/AgengListiyatYono")
